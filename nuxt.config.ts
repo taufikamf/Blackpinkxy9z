f78b94d74bbeb2283dcc7d348d99702452ae70bd
taufikamf <taufikabdul354@gmail.com>
2023-05-03 18:40:52 +0700

    copying it all

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    runtimeConfig: {
+      API_URL: "https://thedramaclubs.com/",
+      public:{
+        API_URL: "https://thedramaclubs.com/"
+      }
+    },
+    css: ['~/assets/css/main.css'],
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+    modules: [
+      [
+        '@pinia/nuxt',
+        {
+          autoImports: ['defineStore', 'acceptHMRUpdate'],
+        },
+      ],
+    ],
+    imports: {
+      dirs: ['stores'],
+    },
+    router:{
+      middleware: [
+        'facebook'
+      ]
+    }
 })
