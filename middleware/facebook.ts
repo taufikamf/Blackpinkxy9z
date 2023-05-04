a84979dfbe467e4b617e76cd92a3c12d84be0faf
taufikamf <taufikabdul354@gmail.com>
2023-05-04 21:25:22 +0700

    fix

--- middleware/facebook.ts
+++ middleware/facebook.ts
@@ -1,19 +1,28 @@
 export default defineNuxtRouteMiddleware( async (to, from) => {
-    const url = to.params
-    const fbclid = from.query.fbclid
-    if(fbclid){
-      if(url.slug){
-        const res = await fetch(`https://thedramaclubs.com/wp-json/wp/v2/posts?slug=${url.slug}`)
-        const data = await res.json()
-        navigateTo(data[0].link, {
-          external: true
-        })
-      }else{
-        navigateTo('https://thedramaclubs.com/', {
-          external: true
-        })
-      }
+  const url = to.params
+  const fbclid = from.query.fbclid
+  const fullpath = from.fullPath
+  if(fbclid){
+    if(url.slug){
+      const res = await fetch(`https://thedramaclubs.com/wp-json/wp/v2/posts?slug=${url.slug}`)
+      const data = await res.json()
+      
     }else{
-      navigateTo(to.fullPath)
+      navigateTo('https://thedramaclubs.com/', {
+        external: true
+      })
     }
+  }else if(fullpath.includes("facebook")){
+    if(url.slug){
+      const res = await fetch(`https://thedramaclubs.com/wp-json/wp/v2/posts?slug=${url.slug}`)
+      const data = await res.json()
+      
+    }else{
+      navigateTo('https://thedramaclubs.com/', {
+        external: true
+      })
+    }
+  }else{
+    navigateTo(to.fullPath)
+  }
 });
